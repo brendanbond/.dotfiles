c6647cd6b3e17efd10f267dbfced282ddff3c1d8
Brendan Bond <brendanjbond@MacBook-BrendanJBond.local>
2022-09-04 17:41:18 -0500

    fix error in naming

---
+++ .clone.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Cloning repositories..."
+
+PROJECTS=$HOME/Projects
+
+# Personal
+git clone git@github.com:brendanbond/react-acceptjs.git $PROJECTS/react-acceptjs
+git clone git@github.com:brendanbond/project-switcher.git $PROJECTS/project-switcher
+git clone git@github.com:brendanbond/pfrange.git $PROJECTS/pfrange
